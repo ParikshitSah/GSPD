7392492021e9cd639c3f1238208dd4e8e28faba6
Parikshit Sah <parikshit.sah12@gmail.com>
2023-01-10 15:20:24 -0500

    Started Testing Driver

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("hello")
-print("Second commit")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_eight_components():
+    
+    driver = webdriver.Chrome()
+
+    driver.get("https://gspd.gosignmeup.com/Public/Course/Browse")
+
+    Finder = driver.find_element(By.CLASS_NAME, "Welcome")
+    print(Finder.text)
+
+    driver.quit()
+
+test_eight_components()
