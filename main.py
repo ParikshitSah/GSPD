2eff80e1e4ff9f36c00c32ef3a766c9d78c63091
Parikshit Sah <parikshit.sah12@gmail.com>
2023-01-11 11:20:37 -0500

    added .env support

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
 import time
+from dotenv import load_dotenv  # for python-dotenv method
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-#os.environ['PATH'] += r"C:/Users/14065/OneDrive - purdue.edu/Documents/GSPD/GSPD"
+
 driver = webdriver.Chrome()
+load_dotenv()  # for python-dotenv method
 
 def main():
-    
 
-
-# Start a browser instance
-    
 
     # Navigate to the login page
     driver.get("http://gspd.gosignmeup.com/admin")
 
     # Find the username and password fields
-    u = driver.find_element(By.TAG_NAME, "form")
-    p = u.find_element(By.NAME, 'name').send_keys("webdriver" )
-    c = u.find_element(By.NAME, 'pass').send_keys("webdriver" + Keys.ENTER)
-
-    
-   
-    # password = driver.find_element(By.ID , "Password")
+    Form = driver.find_element(By.TAG_NAME, "form")
+    User = Form.find_element(By.NAME, 'name').send_keys(
+        os.environ.get('Username_GSMU'))
+    Pass = Form.find_element(By.NAME, 'pass').send_keys(
+        os.environ.get('Password_GSMU') + Keys.ENTER)
 
 
     time.sleep(500)
-  
-    # Enter the username and password
-    
-
-    # Submit the form
-    
 
     
+
+
 main()
