177aed8da9afa529cdee528f974a56056ec8e70a
Parikshit Sah <parikshit.sah12@gmail.com>
2023-01-17 16:16:33 -0500

    Return first and last names in two lists

--- main.py
+++ main.py
@@ -15,32 +15,30 @@ def main():
     driver.get("http://gspd.gosignmeup.com/admin")
 
     # Find the username and password fields
+    #Temporary Method to login > will probably make a branch and merge later
+
     Form = driver.find_element(By.TAG_NAME, "form")
     User = Form.find_element(By.NAME, 'name').send_keys(
         os.environ.get('Username_GSMU'))
     Pass = Form.find_element(By.NAME, 'pass').send_keys(
         os.environ.get('Password_GSMU') + Keys.ENTER)
 
-    #Temporary Method to get names
 
     driver.get("https://gspd.gosignmeup.com/admin/courses_attendance_detail.asp?cid=3786&coursetype=0")
 
-    # Getting First Names
-    # Table > tbody > tr > td > a (First Name)
-
-
-    
+    # Getting Names
 
 
     def get_name(prompt):
         Name = driver.find_elements(By.XPATH, prompt)
-    
+        result = []
         for i in Name:
-            print(i.text)
+            if(i.text != "First Name"):
+                result.append(i.text)
+        return result
     
-    # FirstName = get_name("//table/tbody/tr/td[2]/a") 
-    LastName = get_name("//table/tbody/tr/td[3]/font")
-
+    FirstName = get_name("//table/tbody/tr/td[2]/a") 
+    LastName = get_name("//table/tbody/tr/td[3]//font")
 
 
     time.sleep(500)
