f4e3081e5b682ebdd6747d8fabc72129c8fe9d7b
Parikshit Sah <parikshit.sah12@gmail.com>
2023-01-24 16:27:41 -0500

    Added index to table output

--- Todos.md
+++ Todos.md
@@ -1,9 +1,24 @@
-## Todos 
+# Todos
 
-- [ ] Convert First and Last Name list to dictionary
-- [ ] Integrate File from Replit
-  - [ ] Download all libraries
+- [X] Convert First and Last Name list to dictionary
+- [ ] Check and only scrape when dict is empty
+
+- make a file?
+- object?
+- create a chache file
+
+- [ ] Integrate File from Replit `attendance.py`
+  - [X] Download all libraries -> prettytable
+  - [ ] Fix partial and perfecr matches **side quest**
   - [ ] Make a return value
   - [ ] Remove Print functions
   - [ ] Make a log file _for debugging_
 - [ ] Make a module to get names from excel or zoom
+
+
+
+---
+
+# Resouces
+
+prettytable docs : [Documentation](https://pypi.org/project/prettytable/)
---
+++ attendance.py
@@ -0,0 +1,140 @@
+# Could have used regex and avoided 90% of the stuff done here but YOLO
+
+from prettytable import PrettyTable
+from main import *
+def main():
+
+
+  x = PrettyTable()
+  x.field_names = [
+   "Index","First Name", "Last Name", "⌛  Partial Match", " ✅ Full Match"
+  ]
+  
+  # Conducting a Literature Review 11/1/2022
+
+  gNames = export_list(First_name, Last_name)
+
+
+  #create list with excel names
+  excelNamesUnfiltered = [
+    'Abd Alrhman Bani Issa', 'Abigail Erskine', 'Alex Ozbolt', 'Allegra Stahl',
+    'Connor Lazzaro', 'Elvis Junior Dun-Dery', 'Enoch Tetteh Amoatey',
+    'Enoch Tetteh Amoatey', 'Favour Ojike', 'Francisco', 'Francisco',
+    'Hailey Arreola', 'Heather Howard', 'Ifunanya Ezekiel', 'Ifunanya Ezekiel',
+    'Jak', 'Javid Mardanov', 'Jed Wang', 'JOSEPH ALI',
+    'Kayla Pasteur (kayla pasteur)', 'mberikou',
+    'Mofareh Alzahrani # Alzahrm@purdue.edu', 'Monica Agu', 'Monica Agu',
+    'Nigar Karimli', 'Padde Musa', 'Padde Musa', 'Padde Musa', 'Rajan Mishra',
+    'Rashi Jain', 'Ravishankar Chatta Subramaniam',
+    'Ravishankar Chatta Subramaniam', 'Ravishankar Chatta Subramaniam',
+    'Ravishankar Chatta Subramaniam', 'Ravishankar Chatta Subramaniam',
+    'Ravishankar Chatta Subramaniam', 'Rishi', 'Samarnh Pang', 'Shuqi Liao',
+    'Shuqi Liao', 'Trey Cluff', 'Uche Chidi', 'Uche Chidi (chidi’s iPa)',
+    'Vishwa Chandupatla', 'William Murray Keller', 'Ying Cheng Chen',
+    'Abbas Naseem'
+  ]
+
+  excelNames = [*set(excelNamesUnfiltered)
+                ]  #only unique values from excel names
+
+  splitNames = []
+
+  def find_greater(a, b):
+    if (len(a) > len(b)):
+      return a
+    else:
+      return b
+
+  def find_smaller(a, b):
+    if (len(a) > len(b)):
+      return b
+    else:
+      return a
+
+  KeyValues = {"Matches": {}, "Partial Matches": {}}
+
+  def matchArgs(arg1, arg2):
+    if ((arg2 in arg1) or (arg1 in arg2)):
+      return True
+
+  def find_matches(index):
+    rfirst = []
+    rlast = []
+
+    for i in range(0, len(splitNames)):
+
+      for j in gNames["last"]:
+
+        if (matchArgs(gNames["first"][j].lower(),
+                      splitNames[i].lower()) == True):
+
+          rfirst.append(j)
+
+        if (matchArgs(gNames["last"][j].lower(),
+                      splitNames[i].lower()) == True):
+
+          rlast.append(j)
+
+    return rfirst, rlast
+
+  for index in range(0, len(excelNames)):
+    splitNames = excelNames[index].split()
+    listFirst, listLast = find_matches(index)
+    smallerList = find_smaller(listFirst, listLast)
+    greaterList = find_greater(listFirst, listLast)
+
+    for number in greaterList:
+      if (number in smallerList):
+        if (index in KeyValues["Matches"].keys()):
+          KeyValues["Matches"][index].append(number)
+        else:
+          # print("match:", excelNames[index], "with", gNames["first"][number], gNames["last"][number], number,smallerList, greaterList, index  )
+          KeyValues["Matches"][index] = [number]
+
+      elif (index in KeyValues["Partial Matches"].keys()):
+        KeyValues["Partial Matches"][index].append(number)
+      else:
+        # print("partial match:", excelNames[index], "with", gNames["first"][number], gNames["last"][number], number, index, smallerList, greaterList )
+        KeyValues["Partial Matches"][index] = [number]
+
+  def show_matches():
+    counter = 1
+    #print complete matches
+    for i in KeyValues["Matches"]:
+      for indexofarray in [*set(KeyValues["Matches"][i])]:
+        x.add_row([
+          counter, gNames["first"][indexofarray], gNames["last"][indexofarray], "-",
+          excelNames[i]
+        ])
+        counter = counter + 1
+
+    for i in KeyValues["Partial Matches"]:
+      for indexofarray in [*set(KeyValues["Partial Matches"][i])]:
+        x.add_row([
+          counter, gNames["first"][indexofarray], gNames["last"][indexofarray],
+          excelNames[i], "-"
+        ])
+        counter = counter + 1
+    
+
+    
+
+  show_matches()
+
+  def show_results():
+
+    x.sortby = "Last Name"
+    print(x.get_string(sortby="Index"))
+    print("-" * 60)
+    print("|", "🧮 Total perfect matches:",
+          '{:0>2}'.format(len(KeyValues["Matches"])), "|",
+          "Total partial matches:",
+          '{:0>2}'.format(len(KeyValues["Partial Matches"])), "|")
+    print("-" * 60)
+    # print('📃 KeyValues:', KeyValues)
+
+  show_results()
+
+
+if __name__ == "__main__":
+  main()
