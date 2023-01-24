626e19008b9cb1a7d77d195b64501f0c71f43e6f
Parikshit Sah <parikshit.sah12@gmail.com>
2023-01-24 13:19:38 -0500

    Fully functional first and last names

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import scrape
 import time
 
-last_prompt = "//table/tbody/tr/td[2]"
+last_prompt = "//table/tbody/tr/td[2]//a"
 first_prompt = "//table/tbody/tr/td[3]//font"
 # Enter webpage link here
-link = "https://gspd.gosignmeup.com/admin/courses_attendance_detail.asp?cid=3860" 
+link = "https://gspd.gosignmeup.com/admin/courses_attendance_detail.asp?cid=3834&coursetype=0" 
 
 scrape.login()
 
 First_name = scrape.get_name(first_prompt, link)
 Last_name = scrape.get_name(last_prompt, link)
 
-print(Last_name, len(Last_name))
+print("Last names: ", len(Last_name), "First Names: ", len(First_name))
 
